8beefda8c6981624d0e6b36ebb3d01fb99d7d02f
Yuhi Narita <yuhi.narita26@gmail.com>
2024-04-03 22:46:08 +0800

    Fix bug so that list command also shows list of all groups

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -2,23 +2,25 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_COURSE_MATES;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_GROUPS;
 
 import seedu.address.model.Model;
 
 /**
- * Lists all course mates in the contact list to the user.
+ * Lists all course mates and groups in the contact list to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all course mates";
+    public static final String MESSAGE_SUCCESS = "Listed all course mates and groups";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredCourseMateList(PREDICATE_SHOW_ALL_COURSE_MATES);
+        model.updateFilteredGroupList(PREDICATE_SHOW_ALL_GROUPS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
